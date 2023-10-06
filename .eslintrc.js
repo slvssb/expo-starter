38f1ec8ed72b84e13ab26533a31602259b0fc453
Selva <slvssb08@gmail.com>
2023-10-07 04:01:13 +0530

    update: base configs

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  extends: [
+    'universe/native',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
